cfca7e54cecb9e5146d7de7c6da3fc21f201d40b
Bravestone01 <mujtabashah906@gmail.com>
2024-10-02 16:22:45 +0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [show, setShow]=useState(false)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        show ? <View style={styles.modal}>
+        <View style={styles.body}>
+          <Text>Hello</Text>
+          <Button title='close' onPress={()=>setShow(false)}/>
+
+        </View>
+      </View>:null
+      }
+      <Button title='Show Custom Modal' onPress={()=>setShow(true)}/>
     </View>
   );
 }
@@ -13,8 +23,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"flex-end",
+    marginBottom:10,
   },
+  modal:{
+    flex:1,
+    backgroundColor:"rgba(50,50,50,0.5)",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  body:{
+    backgroundColor:"white",
+    width:300,
+    height:300,
+    padding:20,
+    justifyContent:"center",
+    borderRadius:20,
+    shadowColor:"green",
+    elevation:20,
+
+  }
 });
